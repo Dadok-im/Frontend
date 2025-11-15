963dc13458ba88116a9d1d5c7d98daf78743e8d6
leeminjxun <th554751@naver.com>
2025-11-15 21:12:01 +0900

    백엔드 URL 및 환경 타입 업데이트

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,10 @@
 
 interface ImportMetaEnv {
   readonly VITE_KAKAO_JS_KEY: "1911dddedbb5f270980947917af7df7f";
-  readonly VITE_API_BASE_URL: "http://localhost:8080";
+  // readonly VITE_API_BASE_URL: "http://localhost:8080";
+  // readonly VITE_BACKEND_API_BASE_URL: "http://localhost:8080";
+  readonly VITE_API_BASE_URL: "https://dadokim.o-r.kr";
+  readonly VITE_BACKEND_API_BASE_URL: "https://dadokim.o-r.kr";
 }
 
 interface ImportMeta {
