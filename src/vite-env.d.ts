cdd32cc8af8c004826889326930da869dde4a9b9
leeminjxun <th554751@naver.com>
2025-11-17 17:24:55 +0900

    fix : 백엔드 API 경로 수정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,8 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_KAKAO_JS_KEY: "1911dddedbb5f270980947917af7df7f";
   // readonly VITE_API_BASE_URL: "http://localhost:8080";
   // readonly VITE_BACKEND_API_BASE_URL: "http://localhost:8080";
-  readonly VITE_API_BASE_URL: "https://dadokim.o-r.kr";
-  readonly VITE_BACKEND_API_BASE_URL: "https://dadokim.o-r.kr";
+  readonly VITE_API_BASE_URL: "https://api.dadokim.cloud";
+  readonly VITE_BACKEND_API_BASE_URL: "https://api.dadokim.cloud";
 }
 
 interface ImportMeta {
